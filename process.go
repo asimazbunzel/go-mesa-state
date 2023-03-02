8265279600f28613156f48c7b14773dbfe271fe2
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2023-03-02 11:28:17 -0300

    add methods to get process state and root directory

--- process.go
+++ process.go
@@ -2,12 +2,12 @@ package mesastate
 
 import (
   "bytes"
-  // "bufio"
-	"fmt"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
+  "bufio"
+  "os"
+  "path/filepath"
+  "strconv"
+  "strings"
+  "errors"
 )
 
 // MESAproc contains information on the process running the MESA simulation such as the process id,
@@ -20,20 +20,10 @@ type MESAprocess struct {
 }
 
 // WalkProc function searches for a given `executable_name` in the /proc directory
-func (M *MESAprocess) WalkProc () error {
+func (M *MESAprocess) WalkProc () {
 
-  fmt.Println("searching for process ID of executable:", M.ProcessName)
-  
   // use filepath Walk inside /proc to search for executable_name in each process directory
-  err := filepath.Walk("/proc", M.findMESAProcessID)
-  if err != nil {
-    fmt.Println("error found:", err)
-    return err
-  }
-
-  fmt.Println("here we are:", M)
-
-  return nil
+  filepath.Walk("/proc", M.findMESAProcessID)
 }
 
 // findMESAproc: process with MESA simulation
@@ -44,62 +34,80 @@ func (M *MESAprocess) findMESAProcessID (path string, info os.FileInfo, err erro
   // get only process of the type `/proc/<pid>/status` where <pid> is just a number
   if strings.Count(path, "/") == 3 {
 
-    fmt.Println("looking for process in", path)
-
     // we want the `status` file to search for the executable name
     if strings.Contains(path, "/status") {
-
+    
       // store PID in case it is the right case
       pid, err := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
       if err != nil {
-        return err
+        M.ProcessID = -1
+        return errors.New("process error: could not extract PID")
       }
 
       // read status file. first line contains executable name
       f, err := os.ReadFile(path)
+      if err != nil {
+        M.ProcessID = -1
+        return errors.New("process error: could not read `status` file")
+      }
       processName := string(f[6:bytes.IndexByte(f, '\n')])
-      if processName == M.ProcessName {
-        
-        fmt.Println("found process with name:", processName)
 
+      // matching process name ? return data
+      if processName == M.ProcessName {
         // store PID of founded matching process
         M.ProcessID = pid
+        return filepath.SkipAll
       }
     }
   }
 
   return nil
 }
-      
 
-// fast way to check if file corresponds to executable
-  //     f, err := os.ReadFile(path)
-  //     if err != nil {
-  //       return err
-  //     }
-  //     procName := string(f[6:bytes.IndexByte(f, '\n')])
-  //     if procName == M.ProcName {
-  //
-  //       // set some values
-  //       M.ProcID = pid
-  //       M.ProcName = procName
-  //     
-  //       // open file, this time to get status
-  //       f, err := os.Open(path)
-  //       if err != nil {
-  //         return err
-  //       }
-  //
-  //       // scan whole file
-  //       scanner := bufio.NewScanner(f)
-  // 
-  //       // grab state of process from `status` file
-  //       for scanner.Scan() {
-  //         if strings.Contains(scanner.Text(), "State") {
-  //           state_line := scanner.Text()
-  //           M.State = state_line[7:strings.LastIndex(state_line, "")]
-  //         }
-  //       }
-  //
-  //       // close file
-  //       f.Close()
+// GetMESAProcessState retrieves the state of the process from the `status` file. It must be called
+// after getting the process ID (PID) as it needs the path `/proc/PID/status`
+func (M *MESAprocess) GetMESAProcessState () error {
+
+  // full path to status file
+  statusFilename := "/proc/" + strconv.Itoa(M.ProcessID) + "/status"
+  
+  // open file, this time to get status
+  f, err := os.Open(statusFilename)
+  if err != nil {
+    return err
+  }
+
+  defer f.Close()
+
+  // scan whole file
+  scanner := bufio.NewScanner(f)
+
+  // grab state of process from `status` file
+  for scanner.Scan() {
+    if strings.Contains(scanner.Text(), "State") {
+      stateLine := scanner.Text()
+      M.State = stateLine[7:strings.LastIndex(stateLine, "")]
+      return nil
+    }
+  }
+
+  return errors.New("process error: state not found")
+}
+
+func (M *MESAprocess) GetMESAProcessRootDirectory () error {
+  
+  // full path to status file
+  statusFilename := "/proc/" + strconv.Itoa(M.ProcessID) + "/exe"
+  exe, err := os.Readlink(statusFilename)
+  if err != nil {
+    return err
+  }
+
+  // trim path to get directory only
+  M.RootDirectory = exe[0:strings.LastIndex(exe, "/")]
+
+  // return no error
+  return nil
+}
+
+
