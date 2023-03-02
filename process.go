2781d9ee21195c2b4d3efeafb9a81fe14a8e2734
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2023-03-02 09:48:15 -0300

    changes such that finding the process ID is a separate method of the MESAProcess struct

--- process.go
+++ process.go
@@ -2,7 +2,7 @@ package mesastate
 
 import (
   "bytes"
-  "bufio"
+  // "bufio"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -12,76 +12,94 @@ import (
 
 // MESAproc contains information on the process running the MESA simulation such as the process id,
 // the directory where the executable is located
-type MESAproc struct {
-  ProcID        int    `json:"procid"`
+type MESAprocess struct {
+  ProcessID     int    `json:"procid"`
   RootDirectory string `json:"rootdirectory"`
-  ProcName      string `json:"procname"`
+  ProcessName   string `json:"procname"`
   State         string `json:"state"`
 }
 
 // WalkProc function searches for a given `executable_name` in the /proc directory
-func (M *MESAproc) WalkProc () {
+func (M *MESAprocess) WalkProc () error {
 
-  fmt.Println("searching for executable:", M.ProcName)
+  fmt.Println("searching for process ID of executable:", M.ProcessName)
   
-  // use filepath Walk inside /proc to search for executable_name
-  err := filepath.Walk("/proc", M.findMESAproc)
-
+  // use filepath Walk inside /proc to search for executable_name in each process directory
+  err := filepath.Walk("/proc", M.findMESAProcessID)
   if err != nil {
     fmt.Println("error found:", err)
+    return err
   }
 
+  fmt.Println("here we are:", M)
+
+  return nil
 }
 
 // findMESAproc: process with MESA simulation
 // Idea from this post:
 // https://stackoverflow.com/questions/41060457/golang-kill-process-by-name
-func (M *MESAproc) findMESAproc (path string, info os.FileInfo, err error) error {
+func (M *MESAprocess) findMESAProcessID (path string, info os.FileInfo, err error) error {
 
   // get only process of the type `/proc/<pid>/status` where <pid> is just a number
   if strings.Count(path, "/") == 3 {
 
+    fmt.Println("looking for process in", path)
+
     // we want the `status` file to search for the executable name
     if strings.Contains(path, "/status") {
+
+      // store PID in case it is the right case
       pid, err := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
       if err != nil {
         return err
       }
 
-      // fast way to check if file corresponds to executable
+      // read status file. first line contains executable name
       f, err := os.ReadFile(path)
-      if err != nil {
-        return err
-      }
-      procName := string(f[6:bytes.IndexByte(f, '\n')])
-      if procName == M.ProcName {
+      processName := string(f[6:bytes.IndexByte(f, '\n')])
+      if processName == M.ProcessName {
+        
+        fmt.Println("found process with name:", processName)
 
-        // set some values
-        M.ProcID = pid
-        M.ProcName = procName
-      
-        // open file, this time to get status
-        f, err := os.Open(path)
-        if err != nil {
-          return err
-        }
-
-        // scan whole file
-        scanner := bufio.NewScanner(f)
-  
-        // grab state of process from `status` file
-        for scanner.Scan() {
-          if strings.Contains(scanner.Text(), "State") {
-            state_line := scanner.Text()
-            M.State = state_line[7:strings.LastIndex(state_line, "")]
-          }
-        }
-
-        // close file
-        f.Close()
-
+        // store PID of founded matching process
+        M.ProcessID = pid
       }
     }
   }
+
   return nil
 }
+      
+
+// fast way to check if file corresponds to executable
+  //     f, err := os.ReadFile(path)
+  //     if err != nil {
+  //       return err
+  //     }
+  //     procName := string(f[6:bytes.IndexByte(f, '\n')])
+  //     if procName == M.ProcName {
+  //
+  //       // set some values
+  //       M.ProcID = pid
+  //       M.ProcName = procName
+  //     
+  //       // open file, this time to get status
+  //       f, err := os.Open(path)
+  //       if err != nil {
+  //         return err
+  //       }
+  //
+  //       // scan whole file
+  //       scanner := bufio.NewScanner(f)
+  // 
+  //       // grab state of process from `status` file
+  //       for scanner.Scan() {
+  //         if strings.Contains(scanner.Text(), "State") {
+  //           state_line := scanner.Text()
+  //           M.State = state_line[7:strings.LastIndex(state_line, "")]
+  //         }
+  //       }
+  //
+  //       // close file
+  //       f.Close()
