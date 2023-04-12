a0404280ef3fff0836a7d6eb24f3a000cc8409c8
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2023-04-12 15:20:39 -0300

    add time to logging output

--- log/log.go
+++ log/log.go
@@ -2,17 +2,31 @@ package log
 
 import (
 	"fmt"
+  "time"
+
 	"github.com/TwiN/go-color"
 )
 
+// LogInfo: logging function, INFO level
 func LogInfo(reference, data string) {
-	fmt.Println(color.Ize(color.Green, "["+reference+"] --INFO-- "+data))
+
+  dt := time.Now().Format("01-02-2006 15:04:05.000")
+
+	fmt.Println(color.Ize(color.Green, dt + " --INFO-- " + data + " [" + reference + "]"))
 }
 
+// LogInfo: logging function, ERROR level
 func LogError(reference, data string) {
-	fmt.Println(color.Ize(color.Red, "["+reference+"] --ERROR-- "+data))
+
+  dt := time.Now().Format("01-02-2006 15:04:05.000")
+
+	fmt.Println(color.Ize(color.Red, dt + " --ERROR-- " + data + " [" + reference + "]"))
 }
 
+// LogInfo: logging function, DEBUG level
 func LogDebug(reference, data string) {
-	fmt.Println(color.Ize(color.Yellow, "["+reference+"] --DEBUG-- "+data))
+
+  dt := time.Now().Format("01-02-2006 15:04:05.000")
+
+	fmt.Println(color.Ize(color.Cyan, dt + " --DEBUG-- " + data + " [" + reference + "]"))
 }
